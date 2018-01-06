df6808d948e3ef675f1bd8ef372db0c8017b84ec
Harsh <hrsht.rastogi13@gmail.com>
2018-01-06 00:08:29 -0800

    first commit

---
+++ js/loadPage.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+function showHideDiv(what){document.getElementById(what).style.display=document.getElementById(what).style.display=="block"?"none":"block"}
---
+++ loadPage.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+function showHideDiv(what){alert(what);document.getElementById(what).style.display=document.getElementById(what).style.display=="none"?"block":"none"}
